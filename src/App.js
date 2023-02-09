f48797873028749e6a5251cf09168fefaf7282af
DLNB <smohsinelahi@gmail.com>
2023-02-09 16:04:49 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data";
+import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>
+          {people.length} Birthdays today ({date})
+        </h3>
+        <List people={people} />
+        <button className="btn" onClick={() => setPeople([])}>
+          Clear All
+        </button>
+      </section>
+    </main>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: "Mohsin Elahi",
+    age: 26,
+    image:
+      "https://st4.depositphotos.com/2760050/24301/i/600/depositphotos_243011410-stock-photo-man-with-bristle-on-calm.jpg",
+  },
+  {
+    id: 2,
+    name: "Salman Elahi",
+    age: 30,
+    image:
+      "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg",
+  },
+  {
+    id: 3,
+    name: "Rameez Khan",
+    age: 26,
+    image:
+      "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+  },
+  {
+    id: 4,
+    name: "Moiz Elahi",
+    age: 22,
+    image:
+      "https://img.rawpixel.com/private/static/images/website/2022-05/366-mj-7703-fon-jj.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=f23359a06a626e56bedb45dac2809feb",
+  },
+];
